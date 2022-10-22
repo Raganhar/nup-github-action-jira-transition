5b36c7308c262b05ab43f4d7f84185c47b57bb54
Janus <januspeis@gmail.com>
2022-10-22 10:54:45 +0200

    as

--- nup-github-action-jira-transition/OctoStuff/GitGraph.cs
+++ nup-github-action-jira-transition/OctoStuff/GitGraph.cs
@@ -7,6 +7,25 @@ namespace DotNet.GitHubAction.OctoStuff;
 
 public class GitGraph
 {
+    public async Task listCommitMessagesInPullRequest(string repoowner,string github_token, string repo, int prnumber, string after)
+    {
+        var connection = new Connection(new ProductHeaderValue("bob"), github_token);
+
+        var query = new Query()
+            .RepositoryOwner(repoowner).Repository(repo).PullRequest(prnumber)
+            .Commits(100, after)
+            .Nodes
+            .Select(x => new
+            {
+                Name = x.Commit.Message,
+                BaseRef = x.PullRequest.BaseRefName,
+                HeadRef = x.PullRequest.HeadRefName,
+            });
+
+        var res = await connection.Run(query);
+        Console.WriteLine(JsonConvert.SerializeObject(res,Formatting.Indented));
+    }
+    
     public async Task doStuff(string repoowner,string github_token)
     {
         var connection = new Connection(new ProductHeaderValue("bob"), github_token);
--- tests/OctoTests.cs
+++ tests/OctoTests.cs
@@ -6,6 +6,11 @@ namespace tests;
 public class OctoTests
 {
     [Test]
+    public async Task CommitMessages_in_pr_Test()
+    {
+        await new GitGraph().listCommitMessagesInPullRequest("Raganhar", Utils.GetCredentials().githubToken, );
+    }
+    [Test]
     public async Task NAME_Test()
     {
         await new GitGraph().doStuff("Raganhar", Utils.GetCredentials().githubToken);
