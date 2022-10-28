217fdfd08521ae1a96221a4e8d5a657653723075
Nicklas Peistrup <januspeis@gmail.com>
2022-10-28 20:14:18 +0200

    vov

---
+++ tests/GitCompareTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Octokit;
+
+namespace tests;
+
+public class GitCompareTests
+{
+    [Test]
+    public async Task stuff()
+    {
+        var credentials = Utils.GetCredentials();
+        
+        var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+        var tokenAuth = new Credentials
+        {
+            githubToken = credentials.githubToken
+        }; // NOTE: not real token
+        var res = await github.Repository.Get("Raganhar","nup-github-action-jira-transition");
+
+        Console.WriteLine(res.Id);
+        var comitdiff = await github.Repository.Commit.Compare("Raganhar","nup-github-action-jira-transition","","");
+    }
+}
+
