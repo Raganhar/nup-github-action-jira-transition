f4aa72cf640d716e63613315149614fd4cd3e64c
Nicklas Peistrup <januspeis@gmail.com>
2022-10-29 10:37:32 +0200

    s

---
+++ tests/HandleRevertTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace tests;
+
+public class HandleRevertTests
+{
+    [Test]
+    public async Task Reverted()
+    {
+        new List<string>
+        {
+@"Revert ""Revert ""Merge pull request #2165 from AUTOProff/DEV-877-fe-create-form-containing-gp-options-to-dev-new""
+
+This reverts commit 064f44c81c6e05d5bf845c6936b3822036e5da48.
+6158b5"
+        };
+    }
+}
